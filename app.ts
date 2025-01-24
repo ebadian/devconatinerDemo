952b6f880dc41fea7b2a25c5f3baac62a1a77950
ali.ebadian <ali.ebadian@gmail.com>
2025-01-24 09:34:56 +0000

    adding simple html file

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.render('view/index.html');
 });
 
 app.listen(3000, () => {
